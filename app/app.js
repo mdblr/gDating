703aee32d0c3b9349d9aef970d11d26ef5c993ee
Max Dobler <mdblr@outlook.com>
2016-05-22 23:38:02 -0700

    config sign up, login, and view members routes

--- app/app.js
+++ app/app.js
@@ -7,18 +7,33 @@
 
   function config($routeProvider, $locationProvider) {
     $routeProvider
-      .when('/home', {
-        templateUrl: 'app/partials/home.html',
-        controllerAs: 'Main',
-        controller: 'Main'
+      .when('/', {
+        templateUrl: 'app/partials/landing.html',
+        controllerAs: 'log',
+        controller: 'Register'
+      })
+      .when('/signup', {
+        templateUrl: 'app/partials/signup.html',
+        controllerAs: 'log',
+        controller: 'Register'
+      })
+      .when('/login', {
+        templateUrl: 'app/partials/login.html',
+        controllerAs: 'log',
+        controller: 'Register'
+      })
+      .when('/members', {
+        templateUrl: 'app/partials/all-members.html',
+        controllerAs: 'user',
+        controller: 'User'
       })
       .when('/logout', {
         templateUrl: 'app/partials/logout.html',
-        controllerAs: 'Main',
-        controller: 'Main'
+        controllerAs: 'user',
+        controller: 'User'
       })
       .otherwise({
-        redirectTo: '/home'
+        redirectTo: '/'
       });
 
     $locationProvider.html5Mode(true);
