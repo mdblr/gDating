bf9f22e4b3ce27b04267807245a7af00bb702d5a
Max Dobler <mdblr@outlook.com>
2016-05-23 01:16:09 -0700

    call updated MembersAll factory

--- app/controllers/user.controller.js
+++ app/controllers/user.controller.js
@@ -5,20 +5,17 @@
 
   function User(MembersAll) {
     var vm = this;
-    var test = MembersAll.get();
+    var get_member_profiles = MembersAll.get();
 
-    console.log(test);
-    // console.log(MembersAll.members.get);
-    // MembersAll.get
-    // .then(function(data) {
-    //     console.log(data);
-    //     vm.members = data;
-    //   })
-    // .catch(function(error) {
-    //   vm.members = error;
-    // });
 
-    console.log('user controller')
+    get_member_profiles
+      .then(function(data) {
+        vm.populate = data;
+      })
+      .catch(function(error) {
+        vm.populate = error;
+      });
+
   }
 
 })();
