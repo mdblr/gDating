112ddeae9bbf9a7637b3fcd436f5c6967924ef26
Max Dobler <mdblr@outlook.com>
2016-05-22 23:41:07 -0700

    add factory for getting all members

---
+++ app/services/all-members.factory.js
@@ -0,0 +1,30 @@
+(function() {
+  angular
+    .module('gDating')
+    .factory('MembersAll', MembersAll)
+
+
+  MembersAll.$inject = ['$http'];
+
+  function MembersAll($http) {
+
+    var members = {};
+    var test = [];
+    members.url = 'http://galvanize-student-apis.herokuapp.com/gdating/members';
+    members.get = function () {
+      return $http.get(members.url)
+              .then( function(success) {
+                return success.data;
+              })
+              .catch( function(error) {
+                return error;
+              });
+    }
+
+    return {
+      members
+    }
+  }
+
+
+})();
