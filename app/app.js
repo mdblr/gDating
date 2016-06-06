9e0884175589d47004acf444899c63f25b8e7d31
Max Dobler <mdblr@outlook.com>
2016-06-06 15:55:31 -0700

    config run module

--- app/app.js
+++ app/app.js
@@ -3,9 +3,9 @@
     .module('gDating', ['ngRoute'])
     .config(config);
 
-  config.$inject = ['$routeProvider', '$locationProvider'];
+  config.$inject = ['$routeProvider', '$locationProvider', '$httpProvider']
 
-  function config($routeProvider, $locationProvider) {
+  function config($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'app/partials/landing.html',
@@ -15,17 +15,21 @@
       .when('/signup', {
         templateUrl: 'app/partials/signup.html',
         controllerAs: 'log',
-        controller: 'Register'
+        controller: 'Register',
+        preventWhenLoggedIn: true,
+        signup: true
       })
       .when('/login', {
         templateUrl: 'app/partials/login.html',
         controllerAs: 'log',
-        controller: 'Register'
+        controller: 'Register',
+        preventWhenLoggedIn: true
       })
       .when('/members', {
         templateUrl: 'app/partials/all-members.html',
         controllerAs: 'user',
-        controller: 'User'
+        controller: 'User',
+        restricted: true
       })
       .when('/logout', {
         templateUrl: 'app/partials/logout.html',
@@ -37,5 +41,40 @@
       });
 
     $locationProvider.html5Mode(true);
+    $httpProvider.interceptors.push('AuthInterceptor');
+  }
+})();
+
+(function() {
+  angular
+  .module('gDating')
+  .run(runBlock);
+
+  runBlock.$inject = ['$rootScope', '$location', '$window'];
+
+    // function runBlock($rootScope, $location, $window) {
+    //   $rootScope.$on('$routeChangeStart', function (event, next, current) {
+    //     if (next.restricted && !$window.localStorage.getItem('token')) {
+    //       if (current && current.signup) {
+    //         $location.path('/signup');
+    //       }
+    //       else {
+    //         $location.path('/login');
+    //       }
+    //     }
+    //   });
+    // }
+    function runBlock($rootScope, $location, $window) {
+      $rootScope.$on('$routeChangeStart', function (event, next, current) {
+        // if you try access a restricted page without logging in
+        if (next.restricted && !$window.localStorage.getItem("token")) {
+            $location.path('/login');
+          }
+        }
+        // if you try to log in or sign up once logged in
+        if (next.preventWhenLoggedIn && $window.localStorage.getItem("token")) {
+          $location.path('/users');
+        }
+      });
   }
 })();
