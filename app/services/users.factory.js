9b9f3e3fad97a54fe68fd12794fc611f0bcea953
Max Dobler <mdblr@outlook.com>
2016-06-08 15:11:28 -0700

    add functions to       deleteAccount,       updateProfile,       searchProfiles,       searchBySlug,       and getProfile from back-end api

--- app/services/users.factory.js
+++ app/services/users.factory.js
@@ -1,15 +1,16 @@
 (function() {
   angular
     .module('gDating')
-    .factory('Users', Users)
+    .factory('Users', Users);
 
 
-  Users.$inject = ['$http', '$window'];
+  Users.$inject = ['$http', '$window', '$rootScope'];
 
-  function Users($http, $window) {
+  function Users($http, $window, $rootScope) {
 
     var gDates = 'http://galvanize-student-apis.herokuapp.com/gdating';
     var resource = ['members', 'auth', 'register', 'login'];
+    var member = {};
 
     var everyUser = function () {
       return $http.get(`${gDates}/${resource[0]}`);
@@ -28,11 +29,39 @@
       return $http.post(`${gDates}/${resource[1]}/${resource[3]}`, user);
     }
 
-    var setCurrentUser = function(data) {
-        let {token,user} = data.data
+    var setCurrentUser = function(user) {
+        // let {token,user} = data.data
+        var token = user.data.data.token;
         $window.localStorage.setItem("token",token);
         $window.localStorage.setItem("user",JSON.stringify(user));
-        console.log(token, user, data.data)
+        member = user.data.data.user;
+        //
+        console.log('member', member);
+    }
+
+    var getCurrentUser = function() {
+      // console.log(member.data.user)
+      // return Object.assign(user, data.data)
+    }
+
+    var getProfile = function(user) {
+      $http.get(`${gDates}/${user._id}`);
+    }
+
+    var searchBySlug = function(slug) {
+      $http.get(`${gDates}/search/${slug}`);
+    }
+
+    var searchProfiles = function(query) {
+      $http.get(`${gDates}/search`, query);
+    }
+
+    var updateProfile = function(user) {
+      $http.put(`${gDates}/${user._id}`);
+    }
+
+    var deleteAccount = function(user) {
+      $http.delete(`${gDates}/${user._id}`);
     }
 
     return {
@@ -40,7 +69,14 @@
       signup,
       setCurrentUser,
       logout,
-      login
+      login,
+      getCurrentUser,
+      deleteAccount,
+      updateProfile,
+      searchProfiles,
+      searchBySlug,
+      getProfile
+
     }
 
   }
