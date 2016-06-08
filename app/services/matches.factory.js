b2c4c959edef1472f9720e442feddd096b2a093a
Max Dobler <mdblr@outlook.com>
2016-06-08 15:19:42 -0700

    add getMatches, addMatch, and deleteMatch api calls to back end

---
+++ app/services/matches.factory.js
@@ -0,0 +1,33 @@
+(function() {
+  angular
+    .module('gDating')
+    .factory('Matches', Matches);
+
+    Matches.$inject = ['$http'];
+
+    var gDates = 'http://galvanize-student-apis.herokuapp.com/gdating';
+    var resource = 'matches';
+
+    function Matches($http)
+      {
+      var getMatches = function(user) {
+        $http.get(`${gDates}/${user._id}/${resource}`);
+      };
+
+      var addMatch = function(user) {
+        $http.post(`${gDates}/${user._id}/${resource}`);
+      };
+
+      var deleteMatch = function(user, match_id) {
+        $http.delete(`${gDates}/${user._id}/${resource}/${match_id}`);
+      };
+
+      return {
+        getMatches,
+        addMatch,
+        deleteMatch
+      }
+
+    }
+
+})();
