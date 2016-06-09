4e05b05e1b6e0117be0b099166d6b2c803bb5561
Max Dobler <mdblr@outlook.com>
2016-06-09 15:41:32 -0700

    make dir that toggles features in view (all, popular, search, etc.)

---
+++ app/directives/dashboard.dir.js
@@ -0,0 +1,44 @@
+(function() {
+  angular
+    .module('gDating')
+    .directive('dirDash', dirDash);
+
+    dirDash.$inject = ['$window'];
+
+    function dirDash(Users, $window, $document) {
+
+        var feature, heading, nav, current;
+
+        var directive = {
+          templateUrl: 'app/partials/dashboard.html',
+          link: link
+        }
+
+        return directive;
+
+        function link(scope, element, attrs) {
+
+          heading = document.getElementById('heading');
+          nav = heading.getElementsByTagName('a');
+
+          angular.element($window).ready(function() {
+            current = document.getElementById('all');
+          });
+
+
+          for (var i = 0; i < 5; i++) {
+
+            nav[i].addEventListener('click', function() {
+
+              feature = document.getElementById(this.dataset.users.toString());
+              angular.element(feature).toggleClass('ng-hide');
+
+              //changes current view from default, etc. - see line 22.
+              angular.element(current).toggleClass('ng-hide')
+              current = feature;
+
+            });
+          }
+        }
+      }
+})();
