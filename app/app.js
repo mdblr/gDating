f8d92cffb48c220e90056c8728ad01573264cc77
Max Dobler <mdblr@outlook.com>
2016-06-11 23:54:02 -0700

    remove old controllers

--- app/app.js
+++ app/app.js
@@ -8,13 +8,11 @@
   function config($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'app/partials/landing.html',
-        controllerAs: 'log',
-        controller: 'Register'
+        templateUrl: 'app/partials/landing.html'
       })
       .when('/signup', {
         templateUrl: 'app/partials/signup.html',
-        controllerAs: 'User',
+        controllerAs: 'user',
         controller: 'Register',
         preventWhenLoggedIn: true
       })
@@ -25,15 +23,28 @@
         preventWhenLoggedIn: true
       })
       .when('/members', {
-        templateUrl: 'app/partials/all-members.html',
+        templateUrl: 'app/partials/members-page.html',
+        controllerAs: 'user',
+        controller: 'Member',
+        restricted: true,
+        resolve: {
+          member_profiles: function(Users) {
+            return Users.everyUser()
+            .then(function(users) { return users.data.data } );
+          },
+          current_user: function(Users) {
+            return Users.getCurrentUser();
+          }
+        }
+      })
+      .when('/members/:id', {
+        templateUrl: 'app/partials/landing.html',
         controllerAs: 'user',
         controller: 'User',
         restricted: true
       })
       .when('/logout', {
         templateUrl: 'app/partials/landing.html',
-        controllerAs: 'user',
-        controller: 'User',
         preventWhenLoggedOut: true,
         resolve: {
           logout: function(Users, $location) {
@@ -72,7 +83,8 @@
 
       });
       $rootScope.$on('$routeChangeFailure', function (event, next, current) {
-        $location.path = current;
+        console.log('hey');
+        // $location.path = current;
       });
   }
 })();
